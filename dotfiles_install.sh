c6db8856217f5e6d8de5665ad14104b1ef1fccc4
Artyom K. <artyomkarpov@gmail.com>
2024-09-27 10:02:22 +0300

    save

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -14,7 +14,7 @@ git clone --bare "$DOTFILES" "$HOME/.dotfiles"
 alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 
 mkdir -p .dotfiles-backup && \
-    dotfiles checkout 2>&1 | grep -E "\s+\." | awk "{'print $1'}" | \
+    dotfiles checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | \
     xargs -I{} mv {} .dotfiles-backup/{}
 
 dotfiles checkout --recurse-submodules
