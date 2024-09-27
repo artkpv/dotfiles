ced0444bc7b49dfe6378e895ce79cff2ae436012
Artyom K. <artyomkarpov@gmail.com>
2024-09-27 10:18:48 +0300

    save

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -13,9 +13,10 @@ DOTFILES="$1"
 git clone --bare "$DOTFILES" "$HOME/.dotfiles"
 alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 
-mkdir -p .dotfiles-backup && \
-    dotfiles checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | \
-    xargs -I{} mv {} .dotfiles-backup/{}
+for file in $( dotfiles checkout 2>&1 | grep -E "\s+\." | cut -f 2 ) ; do 
+    mkdir --parents ".dotfiles-backup/$( dirname "$file" )"
+    mv "$file" ".dotfiles-backup/$file"
+done
 
 dotfiles checkout --recurse-submodules
 dotfiles config --local status.showUntrackedFiles no
