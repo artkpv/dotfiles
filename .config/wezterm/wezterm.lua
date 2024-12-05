37a74f53e19cfb58f5b1d6cca6cd845f7af29eee
Artyom K. <artyomkarpov@gmail.com>
2024-12-05 14:56:46 +0300

    save

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = 'GruvboxDark'
-config.hide_tab_bar_if_only_one_tab  = true
+config.color_scheme = "GruvboxDark"
+config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 10.0
 config.window_padding = {
-  left = 2,
-  right = 2,
-  top = 0,
-  bottom = 0,
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
 }
+config.window_close_confirmation = "NeverPrompt"
 return config
